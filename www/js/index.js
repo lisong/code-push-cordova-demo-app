d78fc5259b79a1eb09d836b97202a8ca684aeb89
tablee <lisong2010@gmail.com>
2016-10-19 20:02:37 +0800

    config code-push

--- www/js/index.js
+++ www/js/index.js
@@ -33,6 +33,46 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        /* Invoke sync with the custom options, which enables user interaction.
+           For customizing the sync behavior, see SyncOptions in the CodePush documentation. */
+        window.codePush.sync(
+            function (syncStatus) {
+                switch (syncStatus) {
+                    // Result (final) statuses
+                    case SyncStatus.UPDATE_INSTALLED:
+                        app.displayMessage("The update was installed successfully. For InstallMode.ON_NEXT_RESTART, the changes will be visible after application restart. ");
+                        break;
+                    case SyncStatus.UP_TO_DATE:
+                        app.displayMessage("The application is up to date.");
+                        break;
+                    case SyncStatus.UPDATE_IGNORED:
+                        app.displayMessage("The user decided not to install the optional update.");
+                        break;
+                    case SyncStatus.ERROR:
+                        app.displayMessage("An error occured while checking for updates");
+                        break;
+                    
+                    // Intermediate (non final) statuses
+                    case SyncStatus.CHECKING_FOR_UPDATE:
+                        console.log("Checking for update.");
+                        break;
+                    case SyncStatus.AWAITING_USER_ACTION:
+                        console.log("Alerting user.");
+                        break;
+                    case SyncStatus.DOWNLOADING_PACKAGE:
+                        console.log("Downloading package.");
+                        break;
+                    case SyncStatus.INSTALLING_UPDATE:
+                        console.log("Installing update");
+                        break;
+                }
+            },
+            {
+                installMode: InstallMode.ON_NEXT_RESTART, updateDialog: true
+            },
+            function (downloadProgress) {
+                console.log("Downloading " + downloadProgress.receivedBytes + " of " + downloadProgress.totalBytes + " bytes.");
+            });
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
@@ -45,6 +85,14 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+      // Displays an alert dialog containing a message.
+    displayMessage: function (message) {
+        navigator.notification.alert(
+            message,
+            null,
+            'CodePush',
+            'OK');
     }
 };
 
